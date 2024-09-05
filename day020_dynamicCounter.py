1f983ef64cbfd152fdbf2f57355e64fdc1bc236c
jonaspauls1 <121925348+jonaspauls1@users.noreply.github.com>
2024-09-05 12:32:10 +0200

    Made a for loop with variables to add start, end and increment in the range

---
+++ day020_dynamicCounter.py
@@ -0,0 +1,7 @@
+start = int(input("From where do you want to start counting? "))
+end = int(input("Where do you want to end counting? "))
+increment = int(input("What is the increment? "))
+
+for i in range(start, end, increment):
+  print(i)
+ 
