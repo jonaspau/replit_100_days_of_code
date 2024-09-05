29dfbfc35d679f90bfccd063ef779f1f012316fb
jonaspauls1 <121925348+jonaspauls1@users.noreply.github.com>
2024-09-05 11:42:46 +0200

    Added a number guessing game

---
+++ day018_numberGuessingChallenge.py
@@ -0,0 +1,22 @@
+from random import randint
+
+solution = randint(0, 1000000)
+answer = 0
+attempts = 0
+
+while True:
+  answer = int(input("Guess a number between 1 and 1 000 000: "))
+  if answer != solution:
+    if answer > solution:
+      print("Too high")
+      attempts += 1
+      continue
+    elif answer < solution:
+      print("Too low")
+      attempts += 1
+      continue
+  else:
+    attempts += 1
+    print(f"You guessed it in {attempts} attempts!")
+    print("Thanks for playing. Goodbye...")
+    break
