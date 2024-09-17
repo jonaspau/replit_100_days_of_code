beb9094af27c58886b463e83001adf3af0435c35
jonaspauls1 <121925348+jonaspauls1@users.noreply.github.com>
2024-09-17 10:58:26 +0200

    Updated mokedex.py

--- 046_mokedex.py
+++ 046_mokedex.py
@@ -1,16 +1,45 @@
-import os, time
+import os
+
+
 mokedex = {}
-def prettyPrint():
+
+
+def main():
+  getBeast()
+  printMokedex()
+  
+
+def printMokedex():
   print(f"Name\tType\tHP\tMP")
-  for key, value in mokedex.items():
-    print(f"""{key:^12}|{value["type"]:^10}|{value["hp"]:^6}|{value["mp"]:^6}""")
-while True:
-  print("Add your Beast!")
-  name = input("Name > ").title()
-  type = input("Type > ").title()
-  hp = int(input("HP > "))
-  mp = int(input("MP > "))
-  mokedex[name] = { "type": type, "hp": hp, "mp": mp}
-  print("----------")
-  print()
-  prettyPrint()
+  for beast, stats in mokedex.items():
+    print(f"name: {beast}", end=" | ")
+    for stat, value in stats.items():
+      print(f"{stat}: {value}", end=" | ")
+    print()
+
+
+def getBeast():
+  while True:
+    print("--- Add to MokeDex ---\n")
+    name = input("Enter the beast name: ")
+    element = input("Enter the element: ")
+    special_move = input("Special move: ")
+    starting_hp = input("Starting HP: ")
+    starting_mp = input("Starting MP: ")
+  
+    mokedex[name] = {
+      "element": element,
+      "special move": special_move,
+      "HP": starting_hp,
+      "MP": starting_mp
+    }
+    
+
+    again = input("\nAdd another (y/n)? ").lower()
+    if again == "n":
+      os.system("clear")
+      break      
+    os.system("clear")
+
+
+main()
